4312a76e529ac62f40925b84429f52392a4acec3
Tevils <zelunge@gmail.com>
2018-05-29 16:48:11 +0800

    handle record key

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Records from './components/Records.js';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Records />, document.getElementById('root'));
-registerServiceWorker();
