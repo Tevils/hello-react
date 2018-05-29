0a547d7dfcc0cc5b8d175c483360daf92c0c9e47
Tevils <zelunge@gmail.com>
2018-05-30 01:05:07 +0800

    get data and handle error

--- mydemo/src/components/Records.js
+++ mydemo/src/components/Records.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
-import Record from './Record'
+import Record from './Record';
+import {getJSON} from 'jquery';
 
 class Records extends Component {
   constructor() {
     super();
     this.state={
-      records:[
-        {"id": 1, "date": "2018-01-09", "title": "收入", "amount": 20},
-        {"id": 2, "date": "2018-01-03", "title": "支出", "amount": 10}
-      ]
+      error:null,
+      isLoad:false,
+      records:[]
     }
   }
+  componentDidMount(){
+    getJSON("https://5b0d18bf8126c9001499756a.mockapi.io/api/v1/record").then(
+      response => this.setState({records:response,isLoad:true}),
+      error =>this.setState({isLoad:true,error})
+    )
+  }
   render() {
-    return (
-      <div>
-        <h2>Records</h2>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Title</th>
-              <th>Amount</th>          
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.records.map((record,i)=><Record  key={record.id} record={record}/>)}
-          </tbody>
-        </table>
-      </div>
-    );
+    const {error, isLoad, records}=this.state;
+    if(error){
+      return <div>Error:{error.responseText}</div>
+    }else if(!isLoad){
+      return <div>Loading ...</div>
+    }else{
+      return (
+        <div>
+          <h2>Records</h2>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Title</th>
+                <th>Amount</th>          
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record)=><Record  key={record.id} {...record}/>)}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
   }
 }
 
